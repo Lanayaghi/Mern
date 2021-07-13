d72554441667ad1ef94fb679514efde71801b257
lana yaghi <yaghilana.alaa@gmail.com>
2021-07-13 14:42:54 +0300

    First JSX

---
+++ React/First JSX/project/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return ( 
+    <>
+    <div className="App">
+    <header className="App-header">
+      <h1>Hello Dojo!</h1>
+
+      <p>What I Need To Do :)</p>
+      <ul>
+        <li> Learn react</li>
+        <li> Climb Mt. Everest</li>
+        <li> Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+
+    </header>
+    </div>
+  
+    </>
+  );
+}
+
+export default App;
